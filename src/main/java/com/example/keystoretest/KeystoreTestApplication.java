1ab030012edda51191556561542fe8edcd8aa6a2
MarcVG <marc.vangiel@gmail.com>
2020-04-01 14:49:24 +0200

    Initial check in.

---
+++ src/main/java/com/example/keystoretest/KeystoreTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.keystoretest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeystoreTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeystoreTestApplication.class, args);
+	}
+
+}
